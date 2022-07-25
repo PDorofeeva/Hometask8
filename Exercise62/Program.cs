5383ba6f3a851e36a42b5d940b4d1bd6900e8e5d
Barashnya <polinadorofeeva418@gmail.com>
2022-07-25 13:26:28 +0300

    Exercise62

---
+++ Exercise62/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 62. Заполните спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+1 2 3 4
+12 13 14 5
+11 16 15 6
+10 9 8 7
+*/
+int[,] array = new int[4,4];
+int p = 1;
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    array[0, j] = p;
+    p++;
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        if(i != 0)
+        {
+            array[i,array.GetLength(1) - 1] = p;
+        }
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+}
